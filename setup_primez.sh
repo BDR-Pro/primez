7c6dc5245e0f8f858dc721b86581dca05f6723a6
Bader alotaibi <baderalotaibi3@gmail.com>
2024-03-18 00:08:32 +0300

    fix

--- setup_primez.sh
+++ setup_primez.sh
@@ -6,10 +6,8 @@ BIN_NAME=""
 
 # Check the environment and set the binary name
 if [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "darwin"* ]] || [[ "$OSTYPE" == "freebsd"* ]]; then
-  # Linux, OSX, FreeBSD
   BIN_NAME="prime_linux"
 elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]]; then
-  # Cygwin, MSYS, MinGW, or cmd.exe on Windows
   BIN_NAME="prime.exe"
 else
   echo "Unknown or unsupported operating system. Exiting."
@@ -33,6 +31,12 @@ fi
 
 # Determine the target path
 TARGET_PATH="$BIN_PATH/primez"
+
+# Check if there's a dangling symlink and remove it
+if [ -L "$TARGET_PATH" ] && [ ! -e "$TARGET_PATH" ]; then
+  echo "Removing dangling symlink at $TARGET_PATH"
+  rm "$TARGET_PATH"
+fi
 
 # Always copy the binary to the target path
 cp -f "$EXECUTABLE_PATH" "$TARGET_PATH"
